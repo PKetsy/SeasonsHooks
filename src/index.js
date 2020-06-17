4bd42abb6261be58a3ab6710d0430f1e74a0be91
Phil <K>
2020-06-17 16:02:14 -0500

    initiating SeasonsHooks commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import useLocation from "./useLocation";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message='Please accept location request!' />;
+  }
+
+  return <div className='border red'>{content}</div>;
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
